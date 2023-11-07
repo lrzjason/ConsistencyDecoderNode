f26d59bc0787991f55622a27806dc45b5ae8a899
lrzjason <lrzjason@users.noreply.github.com>
2023-11-07 15:52:39 +0800

    update

--- ConsistencyDecoderLoader.py
+++ ConsistencyDecoderLoader.py
@@ -10,55 +10,40 @@ import torch
 from PIL import Image 
 
 
-
-class ConsistencyDecoderWrapper:
-    def __init__(self, decoder):
-        self.decoder = decoder
-    def decode(self, x):
-        return self.decoder(x)
-
-class ConsistencyDecoderLoader:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {},
-        }
-    RETURN_TYPES = ("VAE",)
-    FUNCTION = "load_consistency_decoder"
-
-    CATEGORY = "ConsistencyDecoderNode"
-
-    def load_consistency_decoder(self):
-        consistencyDecoder = ConsistencyDecoder(device="cuda:0")
-        vae = ConsistencyDecoderWrapper(consistencyDecoder)
-        return (vae,)
-
 class ConsistencyDecode:
     @classmethod
     def INPUT_TYPES(s):
-        return {"required": { "samples": ("LATENT", ), "vae": ("VAE", )}}
+        return {"required": { "samples": ("LATENT", )}}
     RETURN_TYPES = ("IMAGE",)
     FUNCTION = "decode"
 
     CATEGORY = "ConsistencyDecoderNode"
 
-    def decode(self, vae, samples):
-        image = vae.decode(samples["samples"].to("cuda:0"))
-        image = image[0].cpu().numpy()
-        image = (image + 1.0) * 127.5
-        image = image.clip(0, 255).astype(np.uint8)
-        image = Image.fromarray(image.transpose(1, 2, 0))
-        image = image.convert("RGB")
-        image = np.array(image).astype(np.float32) / 255.0
-        image = torch.from_numpy(image)[None,]
-        return (image, )
+    def decode(self, samples):
+        consistencyDecoder = ConsistencyDecoder(device="cuda:0")
+        images = consistencyDecoder(samples["samples"].to("cuda:0")).cpu().numpy()
+        # create result torch tensor
+        result = torch.zeros(images.shape[0],images.shape[2],images.shape[3],images.shape[1]).cpu()
+        # print('value',images.shape[0],images.shape[2],images.shape[3],images.shape[1])
+        # print('image.shape',result.shape)
+        # print('result.shape',result.shape)
+        # loop images
+        for i in range(len(images)):
+            image = (images[i] + 1.0) * 127.5
+            image = image.clip(0, 255).astype(np.uint8)
+            image = Image.fromarray(image.transpose(1, 2, 0))
+            image = image.convert("RGB")
+            image = np.array(image).astype(np.float32) / 255.0
+            image = torch.from_numpy(image)[None,]
+
+            # put image in result
+            result[i] = image
+        return (result, )
 
 NODE_CLASS_MAPPINGS = {
-    "ConsistencyDecoderLoader": ConsistencyDecoderLoader,
     "ConsistencyDecode": ConsistencyDecode
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "ConsistencyDecoderLoader": "ConsistencyDecoderLoader",
     "ConsistencyDecode": "ConsistencyDecode",
 }
